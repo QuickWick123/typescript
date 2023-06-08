fac192297243c5a93fb092fd10cd3a3307f070aa
Quickwick123 <gameregistery@hotmail.com>
2023-06-08 07:27:08 -0400

    typescript

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level;
+level = 1;
+level = 'a';
+
+function render(document: any) {
+    console.log(document);
+}
+
+let numbers: number[] = [1, 2, 3];
+
+numbers.forEach(n => n.toString)
+
+
+//1, 'Mosh'
+let user: [number, string] = [1, 'Mosh']
+user.push(1);
+
+const small = 1;
+const medium = 2;
+const large = 3;
+
+enum Size { Small, Medium, Large };
