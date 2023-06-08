ce463a8511c82c02088f00847049208495cb9383
Quickwick123 <gameregistery@hotmail.com>
2023-06-08 07:27:21 -0400

    typescript

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+level = 1;
+level = 'a';
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+numbers.forEach(n => n.toString);
+let user = [1, 'Mosh'];
+//# sourceMappingURL=index.js.map
